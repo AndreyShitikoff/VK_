d03000d8f44acf6afa6bb6e88b968a054c26a51a
Andrey Shitikov <andrey.shitikoff@gmail.com>
2021-03-04 09:31:51 +0300

    Added Manager

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,21 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Post;
+
+public class PostManager {
+    private Post[] posts;
+
+    public Post[] getPostsForFeed;(){
+        // TODO: add logic
+        return null;
+    }
+
+    public Post[] search (int owner_id, String domain, String query,boolean ownersOnly, int cuunt, int offset){
+        // TODO: add logic
+        return null;
+    }
+
+    public void delete (int owner_id, int post_id){
+        // TODO: add logic
+    }
+}
