b36d2abded4333b212515df07976326e188b7c5e
Andrey Shitikov <andrey.shitikoff@gmail.com>
2021-03-09 15:27:13 +0300

    Updated PostManager

--- src/main/java/ru/netology/manager/PostManager.java
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -5,7 +5,7 @@ import ru.netology.domain.Post;
 public class PostManager {
     private Post[] posts;
 
-    public Post[] getPostsForFeed;(){
+    public Post[] getPostsForFeed(){
         // TODO: add logic
         return null;
     }
